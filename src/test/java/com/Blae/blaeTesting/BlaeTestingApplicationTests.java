e3d0c5eac144cdea5f52d77943b506b4dfa05e07
Emmanuel <blaemedia26@gmail.com>
2024-02-11 23:12:44 -1200

    SpringbootPlayingground

---
+++ src/test/java/com/Blae/blaeTesting/BlaeTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blae.blaeTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaeTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
